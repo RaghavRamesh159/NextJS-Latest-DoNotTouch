c239788a51f6c6117dd365b16ae58184d07f644a
Raghavendran R <47601578+RaghavRamesh159@users.noreply.github.com>
2025-03-12 15:05:21 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 import "../styles/global.css";
 import "../styles/layout.css";
 
+export async function generateStaticParams() { 
+  return []
+}
 export const revalidate = 120
+export const dynamicParams = true
 
 export default function App({ children }) {
   return (
